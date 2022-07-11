65852103ef61993961c29ff4bbcec8ba8d72ca00
minerKtracer <104286945+minerKtracer@users.noreply.github.com>
2022-07-11 14:31:44 +0800

    Tools Inserted

---
+++ PDF Resume Creator/PDF Resume Creator/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace PDF_Resume_Creator
+{
+    partial class PDFGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TXT2 = new System.Windows.Forms.TextBox();
+            this.TXT3 = new System.Windows.Forms.TextBox();
+            this.TXT4 = new System.Windows.Forms.TextBox();
+            this.TXT1 = new System.Windows.Forms.TextBox();
+            this.TXT5 = new System.Windows.Forms.TextBox();
+            this.LB2 = new System.Windows.Forms.Label();
+            this.LB3 = new System.Windows.Forms.Label();
+            this.LB5 = new System.Windows.Forms.Label();
+            this.LB4 = new System.Windows.Forms.Label();
+            this.LB6 = new System.Windows.Forms.Label();
+            this.LB1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TXT2
+            // 
+            this.TXT2.Location = new System.Drawing.Point(50, 173);
+            this.TXT2.Multiline = true;
+            this.TXT2.Name = "TXT2";
+            this.TXT2.Size = new System.Drawing.Size(265, 107);
+            this.TXT2.TabIndex = 0;
+            // 
+            // TXT3
+            // 
+            this.TXT3.Location = new System.Drawing.Point(50, 323);
+            this.TXT3.Multiline = true;
+            this.TXT3.Name = "TXT3";
+            this.TXT3.Size = new System.Drawing.Size(265, 106);
+            this.TXT3.TabIndex = 1;
+            // 
+            // TXT4
+            // 
+            this.TXT4.Location = new System.Drawing.Point(387, 173);
+            this.TXT4.Multiline = true;
+            this.TXT4.Name = "TXT4";
+            this.TXT4.Size = new System.Drawing.Size(265, 107);
+            this.TXT4.TabIndex = 2;
+            // 
+            // TXT1
+            // 
+            this.TXT1.Location = new System.Drawing.Point(321, 54);
+            this.TXT1.Multiline = true;
+            this.TXT1.Name = "TXT1";
+            this.TXT1.Size = new System.Drawing.Size(215, 72);
+            this.TXT1.TabIndex = 3;
+            // 
+            // TXT5
+            // 
+            this.TXT5.Location = new System.Drawing.Point(387, 323);
+            this.TXT5.Multiline = true;
+            this.TXT5.Name = "TXT5";
+            this.TXT5.Size = new System.Drawing.Size(265, 106);
+            this.TXT5.TabIndex = 4;
+            // 
+            // LB2
+            // 
+            this.LB2.AutoSize = true;
+            this.LB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB2.Location = new System.Drawing.Point(136, 76);
+            this.LB2.Name = "LB2";
+            this.LB2.Size = new System.Drawing.Size(179, 29);
+            this.LB2.TabIndex = 5;
+            this.LB2.Text = "Basic Details :";
+            // 
+            // LB3
+            // 
+            this.LB3.AutoSize = true;
+            this.LB3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB3.Location = new System.Drawing.Point(47, 145);
+            this.LB3.Name = "LB3";
+            this.LB3.Size = new System.Drawing.Size(282, 25);
+            this.LB3.TabIndex = 6;
+            this.LB3.Text = "Educational Background :";
+            // 
+            // LB5
+            // 
+            this.LB5.AutoSize = true;
+            this.LB5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB5.Location = new System.Drawing.Point(382, 145);
+            this.LB5.Name = "LB5";
+            this.LB5.Size = new System.Drawing.Size(97, 25);
+            this.LB5.TabIndex = 7;
+            this.LB5.Text = "Skills  : ";
+            // 
+            // LB4
+            // 
+            this.LB4.AutoSize = true;
+            this.LB4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB4.Location = new System.Drawing.Point(47, 293);
+            this.LB4.Name = "LB4";
+            this.LB4.Size = new System.Drawing.Size(187, 25);
+            this.LB4.TabIndex = 8;
+            this.LB4.Text = "Achievements  : ";
+            // 
+            // LB6
+            // 
+            this.LB6.AutoSize = true;
+            this.LB6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB6.Location = new System.Drawing.Point(382, 293);
+            this.LB6.Name = "LB6";
+            this.LB6.Size = new System.Drawing.Size(270, 25);
+            this.LB6.TabIndex = 9;
+            this.LB6.Text = "Character References  : ";
+            // 
+            // LB1
+            // 
+            this.LB1.AutoSize = true;
+            this.LB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB1.Location = new System.Drawing.Point(218, 9);
+            this.LB1.Name = "LB1";
+            this.LB1.Size = new System.Drawing.Size(261, 29);
+            this.LB1.TabIndex = 10;
+            this.LB1.Text = "PDF Resume Creator";
+            this.LB1.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // PDFGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 458);
+            this.Controls.Add(this.LB1);
+            this.Controls.Add(this.LB6);
+            this.Controls.Add(this.LB4);
+            this.Controls.Add(this.LB5);
+            this.Controls.Add(this.LB3);
+            this.Controls.Add(this.LB2);
+            this.Controls.Add(this.TXT5);
+            this.Controls.Add(this.TXT1);
+            this.Controls.Add(this.TXT4);
+            this.Controls.Add(this.TXT3);
+            this.Controls.Add(this.TXT2);
+            this.MaximizeBox = false;
+            this.Name = "PDFGen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TXT2;
+        private System.Windows.Forms.TextBox TXT3;
+        private System.Windows.Forms.TextBox TXT4;
+        private System.Windows.Forms.TextBox TXT1;
+        private System.Windows.Forms.TextBox TXT5;
+        private System.Windows.Forms.Label LB2;
+        private System.Windows.Forms.Label LB3;
+        private System.Windows.Forms.Label LB5;
+        private System.Windows.Forms.Label LB4;
+        private System.Windows.Forms.Label LB6;
+        private System.Windows.Forms.Label LB1;
+    }
+}
+
---
+++ PDF Resume Creator/PDF Resume Creator/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDF_Resume_Creator
+{
+    public partial class PDFGen : Form
+    {
+        public PDFGen()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        }
+    }
+}
