0c42122de41626028362e5c145bbe9024eab2398
minerKtracer <104286945+minerKtracer@users.noreply.github.com>
2022-07-20 16:22:10 +0800

    Apply Image

--- PDF Resume Creator/PDF Resume Creator/Form1.cs
+++ PDF Resume Creator/PDF Resume Creator/Form1.cs
@@ -13,6 +13,8 @@ using iTextSharp.text;
 using iTextSharp.text.pdf;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using iTextSharp.text.html;
+using iTextSharp.text.html.simpleparser;
 
 namespace PDF_Resume_Creator
 {
@@ -88,7 +90,7 @@ namespace PDF_Resume_Creator
                 PDFResume convert = JsonConvert.DeserializeObject<PDFResume>(jsonReadFile);
                 FirstName1.Text = convert.FirstName + " " + convert.LastName;
                 BasicDetails3.Text = convert.PhoneNumber + "\n" + convert.Email + "\n" + convert.Address + "\n";
-                Education8.Text = convert.Year + " " + convert.Course + "\n" + convert.School1 + "\n" + convert.Address1 + "\n" + "\n" + convert.Year1 + " " + convert.School2 + "\n" + convert.Strand + "\n" + convert.Address2 + "\n" + "\n" + convert.Year2 + " " + convert.School3 + "\n" + convert.Address3 + "\n" + "\n" + convert.Year3 + " " + convert.School4 + "\n" + convert.Address4 + "\n";
+                Education8.Text = convert.Year + " " + convert.Course + "\n" + convert.School1 + "\n" + convert.Address1 + "\n" + convert.Year1 + " " + convert.School2 + "\n" + convert.Strand + "\n" + convert.Address2 + "\n" + convert.Year2 + " " + convert.School3 + "\n" + convert.Address3 + "\n" + convert.Year3 + " " + convert.School4 + "\n" + convert.Address4 + "\n";
                 skills5.Text = convert.Skill1 + "\n" + convert.Skill2 + "\n" + convert.Skill3 + "\n" + convert.Skill4 + "\n" + convert.Skill5 + "\n";
                 achievements6.Text = convert.Achievement1 + "\n" + convert.Achievement2 + "\n" + convert.Achievement3 + "\n" + convert.Achievement4 + "\n";
                 CharR.Text = convert.Name + "\n" + convert.Specification + "\n" + convert.PNumber + "\n";
@@ -104,17 +106,42 @@ namespace PDF_Resume_Creator
             Document docx = new Document();
             PdfWriter.GetInstance(docx, new FileStream(@"C:\Users\kylene shane varona\Desktop\ASSIGNMENT 6\PDF Resume Creator\PDF Resume Creator\pdfresume.pdf", FileMode.Create));
             docx.Open();
+            
+            iTextSharp.text.Image jpg = iTextSharp.text.Image.GetInstance(@"C:\Users\kylene shane varona\Desktop\ASSIGNMENT 6\PDF Resume Creator\PDF Resume Creator\ky.jpg");
+
+            //Resize image depend upon your need
+
+            jpg.ScaleToFit(140f, 120f);
+
+            //Give space before image
+
+            jpg.SpacingBefore = 10f;
+
+            //Give some space after the image
+
+            jpg.SpacingAfter = 1f;
+
+            jpg.Alignment = Element.ALIGN_RIGHT;
+
+            jpg.Border = iTextSharp.text.Rectangle.BOX;
+
+            jpg.BorderColor = iTextSharp.text.BaseColor.BLACK;
+
+            jpg.BorderWidth = 3.5F;
+
             Chunk Sline = new Chunk("______________________________________________________________________________");
             Paragraph FirstName = new Paragraph(FirstName1.Text, FontFactory.GetFont("Arial", 25));
-            Paragraph BD = new Paragraph(BasicDetails3.Text, FontFactory.GetFont("Arial", 8));
-            Paragraph edu = new Paragraph("Education", FontFactory.GetFont("Arial", 20));
+            Paragraph BD = new Paragraph(BasicDetails3.Text, FontFactory.GetFont("Arial", 10));
+            Paragraph edu = new Paragraph("Education", FontFactory.GetFont("Arial", 18));
             Paragraph ed = new Paragraph(Education8.Text, FontFactory.GetFont("Arial", 12));
-            Paragraph skills = new Paragraph("Skills", FontFactory.GetFont("Arial", 20));
+            Paragraph skills = new Paragraph("Skills", FontFactory.GetFont("Arial", 16));
             Paragraph skill = new Paragraph(skills5.Text, FontFactory.GetFont("Arial", 12));
-            Paragraph ACS = new Paragraph("Achievements", FontFactory.GetFont("Arial", 20));
+            Paragraph ACS = new Paragraph("Achievements", FontFactory.GetFont("Arial", 16));
             Paragraph AC = new Paragraph(achievements6.Text, FontFactory.GetFont("Arial", 12));
-            Paragraph CRS = new Paragraph("Character References", FontFactory.GetFont("Arial", 20));
+            Paragraph CRS = new Paragraph("Character References", FontFactory.GetFont("Arial", 15));
             Paragraph CR = new Paragraph(CharR.Text, FontFactory.GetFont("Arial", 12));
+
+            docx.Add(jpg);
             docx.Add(FirstName);
             docx.Add(BD);
             docx.Add(Sline);
